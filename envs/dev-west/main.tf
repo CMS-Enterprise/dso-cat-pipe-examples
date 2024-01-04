6ea279f979d1d3359a20e6b8e4f10baa26fc10e8
swaters23 <swaters@revacomm.com>
2024-01-04 14:27:57 -0600

    added random uuid modifier for bucket name

--- envs/dev-west/main.tf
+++ envs/dev-west/main.tf
@@ -3,9 +3,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "random_uuid" "test" {
+}
+
 # Create a S3 bucket
 resource "aws_s3_bucket" "terraform_state" {
-  bucket		  = var.bucket_name
+  bucket		  = "${random_uuid.test.result}-${var.bucket_name}"
   
   lifecycle {
     prevent_destroy = false
