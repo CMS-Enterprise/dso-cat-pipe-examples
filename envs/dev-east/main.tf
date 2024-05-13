09854d09b26670eb6f10151a242ac4818b127ab3
SJKQ-DSO <abdulqavi.shaik@cms.hhs.gov>
2024-05-13 17:38:57 -0400

    added dev-east variables

---
+++ envs/dev-east/main.tf
@@ -0,0 +1,16 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_uuid" "test" {
+}
+
+# Create a S3 bucket
+resource "aws_s3_bucket" "terraform_state" {
+  bucket		  = "${random_uuid.test.result}-${var.bucket_name}"
+  
+  lifecycle {
+    prevent_destroy = false
+  }
+}
---
+++ envs/dev-east/variables.tf
@@ -0,0 +1,5 @@
+variable "bucket_name" {
+  type        = string
+  description = "The name of the bucket that is being created with terraform."
+  default     = "test-bucket-5678"
+}
